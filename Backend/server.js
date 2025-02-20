c33ea6b3e277da2309d7fd6be057f44ebce8a104
ArthurJuma98 <arthurjuma98@gmail.com>
2025-02-20 13:06:00 +0300

    server set up

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+require ("dotenv").config();
+
+const express = require('express');
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//connect to Database
+
+connectDB();
+
+//routes
+
+app.use("/api/auctions", require("./routes/auctionRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
